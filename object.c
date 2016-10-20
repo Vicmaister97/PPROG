8553ab74eb8e45e6539e995383a9d2eeadd5a431
Victor Garcia Carrera <victorgarciacarrera@gmail.com>
2016-10-20 19:56:38 +0200

    Funcion crear modelo

--- object.c
+++ object.c
@@ -8,3 +8,14 @@ struct _Object{
 	int *properties; /*[0]: Strength; [1]: HP; [2]: Speed; [3]: Wisdom; [4]: Defense*/
 	int location;
 }
+
+Object* create_objet (FILE *fp){
+	Object *po = (Object *) malloc (sizeof (Object));
+	if (po == NULL){
+		return;
+	}
+	char buf[100];
+	po->id = atoi(fgets(buf, 100, fp));
+	fgets(buf, 100, fp)
+	buf[strlen(buf)-1] = 0;
+	po->name = strdup(buf);
