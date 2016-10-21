68da0437843fc3b17d57f083aa1b324266a83871
Victor Garcia <victorgarciacarrera@gmail.com>
2016-10-21 18:02:49 +0200

    Implementacion de obj.c v1.0

--- object.h
+++ object.h
@@ -7,12 +7,11 @@
 typedef struct _Object Object;
 
 /*struct _Object{
-	int id;
-	Bool isPicked;
+	int id; //id >= 0
 	char *name;
 	char *desc;
-	int *properties; /*[0]: Strength; [1]: HP; [2]: Speed; [3]: Wisdom; [4]: Defense*/
-/*	int location;
+	int *properties; [0]: // Strength; [1]: HP; [2]: Speed; [3]: Wisdom; [4]: Defense
+	int location; // If location is -1, we say that the object is in the Inventory of the player, is picked
 }
 */
 
@@ -20,13 +19,14 @@ Object* create_objet (FILE *fp); /* Function that creates an object reading from
 void delete_object (Object *po); /* Function that destroys an object */
 Bool isInInventory (Object *po); /* Function that tells you if an object is in the player inventory or not */
 Status move_object (Object *po, int location); /* Function that moves an object to a location (space) */
-Bool isUsable_object (Object *po, );  /* Function that tells you if you are able to use an object or not */
-int getId_object (Object *po);   /* Function that returns the ID an object */
-Status setName_object (Object *po, char *name);  /* Function that changes the name of an object */
-char* getName_object (Object *po);  /* Function that returns the name of an object */
-char* desc_object (Object *po);   /* Function that returns the description of an object */
-Status setProp_object (Object *po, int *pi);  /* Function that changes the properties of an object */
-int* getProp_object (Object *po);  /* Function that returns the properties of an object */
-int getLocation_object (Object *po);  /* Function that returns the location of an object */
+Object* pick_object (Object *po); /*Function that picks an object (so the player can use it) */
+Bool isUsable_object (Object *po); /* Function that tells you if you are able to use an object or not */
+int getId_object (Object *po); /* Function that returns the ID an object */
+Status setName_object (Object *po, char *name); /* Function that changes the name of an object */
+char* getName_object (Object *po); /* Function that returns the name of an object */
+char* desc_object (Object *po); /* Function that returns the description of an object */
+Status setProp_object (Object *po, int *pi); /* Function that changes the properties of an object */
+int* getProp_object (Object *po); /* Function that returns the properties of an object */
+int getLocation_object (Object *po); /* Function that returns the location of an object */
 
 #endif
