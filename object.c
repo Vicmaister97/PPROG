51691ca0a1b08ed287503888172815a3431b04f3
blancams <blancamartinselgas@gmail.com>
2016-12-23 10:19:47 +0100

    game

--- object.c
+++ object.c
@@ -4,12 +4,10 @@
 #include <string.h>
 #include "object.h"
 
-
 struct _Object{
 	int id; /* id >= 0 */
 	char *name;
 	char *desc;
-	char *long_desc;
 	int *properties; /* [0]: Strength; [1]: Endurance; [2]: HP; [3]: Speed; [4]: Agility;  [5]: Luck */
 	int location;
 	Bool picked;
@@ -36,9 +34,6 @@ Object* create_object (FILE *fp){ /*Fuction that creates and allocs memory for a
 	fgets(buf, 100, fp);
 	po->desc = (char *)malloc(sizeof(char)*(strlen(buf)+1));
 	strcpy(po->desc, buf);
-	fgets(buf, 100, fp);
-	po->long_desc = (char *)malloc(sizeof(char)*(strlen(buf)+1));
-	strcpy(po->long_desc, buf);
 	fgets(buf, 100, fp);
 	
 	/*for (pt = buf, nsp = 0; *pt; pt++){ Podría sustituirse por poner directamente nsp = 4 si al final hay sólo 4 propiedades 
@@ -94,42 +89,39 @@ void delete_object (Object *po){ /*Fuction that receives an object and deletes i
 
 	free (po->name);
 	free (po->desc);
-	free (po->long_desc);
 	free (po->properties);
 	free (po);
 	return;
 }
 
 Bool isInInventory (Object *po){ /*Function that returns wether an object is in the Inventory of the player or not*/
-	if(po && po->picked == TRUE) return TRUE;
+	if(po && po->location == -1) return TRUE;
 	return FALSE;
 }
 
-Status move_object (Object *po, int location, int col, int row){ /* XXX Function that moves an object to a given location(space)*/
+Status move_object (Object *po, int location){ /*Function that moves an object to a given location(space)*/
 	if (po == NULL || location < -1)
 		return ERROR;
 
 	po->location = location;
-	po->col = col;
-	po->row = row;
 
 	return OK;
 }
 
-Object* pick_object (Object *po){ /* XXX Function that moves a given object to the Inventory of the player*/
+Object* pick_object (Object *po){ /*Function that moves a given object to the Inventory of the player*/
 	if (po == NULL)
 		return NULL;
 
-	po->picked = TRUE;
+	po->location = -1;
 
 	return po;
 }
 
-/*Status drop_object(Object *po){
+Status drop_object(Object *po){
 	if(!po) return ERROR;
 	po->location = 0;
 	return OK;
-}*/
+}
 
 /*Bool isUsable_object (Object *po){  Function that tells you if you are able to use a given object or not
 	if (po == NULL)
@@ -170,7 +162,7 @@ char* getDesc_object (Object *po){ /*Function that returns the description of a 
 	if (po == NULL)
 		return NULL;
 
-	return po->long_desc;
+	return po->desc;
 }
 
 Status setProp_object (Object *po, int prop, int nv){ /*Function that changes a certain property of a given object to a given value*/
@@ -196,7 +188,7 @@ int getLocation_object (Object *po){ /*Function that returns the location of a g
 }
 
 char getPicture_object (Object *po){
-	if(!po) return ' '; /*NO SERÍA + FÁCIL NULL??*/
+	if(!po) return ' ';
 	return po->picture;
 }
 
@@ -212,9 +204,7 @@ int getCol_object (Object *po){
 	return po->col;
 }
 
-Status drop_object(Object *po){
-	if(!po) return ERROR;
-
-	po->picked == FALSE;
-	return OK;
+Status use_object (Object *po){
+	
 }
+
