18747783507bc68922545b9440c4c971140ade0d
blancams <blancamartinselgas@gmail.com>
2017-01-22 17:10:23 +0100

    h

--- hola/space.h
+++ hola/space.h
@@ -11,7 +11,6 @@ int getID_Space(Space *s);/*we obtain space id which help us identify it*/
 
 int getID_Space(Space *s);/*we obtain space id which help us identify it*/
 
-/*obtain different and complementary descriptions of the space*/
 Bool isLocked_Space(Space *s, int direction);/*tell us if we need some special stuf(RELATE TO object.c) to be able to move there*/
 Space *create_Space(FILE *f);
 int pictRows_Space(Space *s);
