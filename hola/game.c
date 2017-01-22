4a2a80ed82ed2417a870999193123acbeaa7d2af
blancams <blancamartinselgas@gmail.com>
2017-01-22 16:01:22 +0100

    smth

--- hola/game.c
+++ hola/game.c
@@ -97,12 +97,6 @@ static void prepare_game(Game *gm){
 	char enemy[num_enemy];
 	Player **enemies = getEnemiesSpace_world(gm->w,getWaI_player(getPlayer_world(gm->w)));
 
-	
-	char buf[20];
-	sprintf(buf, "%d!!!!!!!!!!!", getWaI_player(getPlayer_world(gm->w)));
-	extra_write_message_object_intrf(gm->ic, buf);
-	
-
 	for( ; i < num_enemy; i++){
 	    enemy[i] = getSymbol_player(enemies[i]);
 	    enemy_row[i] = getRow_player(enemies[i]);
@@ -537,7 +531,6 @@ void play_game(Game *gm){
 	while(1){
 		prepare_to_write_cmd_intrf(gm->ic);
 		if(isOnDoor_intrf(gm->ic)){
-			extra_write_message_object_intrf(gm->ic, "sip");
 			aux = - _read_key();
 			if(aux >= 0){
 				if(ret == aux){
